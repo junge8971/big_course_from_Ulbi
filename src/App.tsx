0bd748103117d471251a84c15e0b37a396b9572d
junge8971 <junge8971@gmail.com>
2024-07-22 14:29:08 +0300

    Реализована функциональность управления темами

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import "./index.scss";
+import "./style/index.scss";
 
 import { FC, Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 
-import Counter from "./components/Counter";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { useTheme } from "./theme/useTheme";
 
 export const App: FC = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Сменить тему</button>
       <Link to="/">Главная</Link>
       <Link to="/about">О нас</Link>
 
---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,27 @@
+import { FC, ReactNode, useMemo, useState } from "react";
+
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./themeContext";
+
+const defaultTheme =
+  (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.light;
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export const ThemeProvider: FC<ThemeProviderProps> = ({ children }) => {
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+  const defaultProps = useMemo(
+    () => ({
+      theme: theme,
+      setTheme: setTheme,
+    }),
+    [theme]
+  );
+  return (
+    <ThemeContext.Provider value={defaultProps}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
