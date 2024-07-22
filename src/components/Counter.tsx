7638887637e389a785fb90ba70cd6da8f7b6f5e7
junge8971 <junge8971@gmail.com>
2024-07-22 10:41:37 +0300

    Подключен реакт, добавлены обработчики для стилей

---
+++ src/components/Counter.tsx
@@ -0,0 +1,20 @@
+import "./Counter.scss";
+
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>increment</button>
+    </div>
+  );
+};
+
+export default Counter;
--- webpack.config.ts
+++ webpack.config.ts
@@ -12,7 +12,7 @@ export default (env: BuildEnv) => {
   const port = env.port || 3000;
 
   const paths: BuildPaths = {
-    entry: path.resolve(__dirname, "src", "index.ts"),
+    entry: path.resolve(__dirname, "src", "index.tsx"),
     build: path.resolve(__dirname, "dist"),
     html: path.resolve(__dirname, "public", "index.html"),
   };
